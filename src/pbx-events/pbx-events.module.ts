c033eb3e284aefff7ff18b095790823618abe0b2
Leroy Anders <leroyanders@Leroys-MacBook-Air.local>
2024-04-03 13:57:26 +0300

    quick changes

--- src/pbx-events/pbx-events.module.ts
+++ src/pbx-events/pbx-events.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { QueueModule } from '../queue/queue.module';
 import { PbxEventsGateway } from './pbx-events.gateway';
+import { YeastarService } from '../yeastar/yeastar.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [QueueModule],
-  providers: [PbxEventsGateway],
+  imports: [QueueModule, HttpModule],
+  providers: [PbxEventsGateway, YeastarService],
   exports: [PbxEventsGateway],
 })
 export class PbxEventsModule {}
